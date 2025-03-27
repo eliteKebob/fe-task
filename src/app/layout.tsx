3af4b87c12e2349a0501b79c8451b8f08c8afc0f
Kaan <kaanozmendeveloper@gmail.com>
2025-03-28 00:55:38 +0300

    feat(home): homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import ThemeProvider from "@/providers/theme";
+import StoreProvider from "@/providers/store";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500"],
 });
 
 export const metadata: Metadata = {
@@ -23,9 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="tr">
+      <body className={`${poppins.variable}`}>
+        <AppRouterCacheProvider>
+          <StoreProvider>
+            <ThemeProvider>
+              <Header />
+              {children}
+              <Footer />
+            </ThemeProvider>
+          </StoreProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );

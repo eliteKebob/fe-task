845cd4d7686aae5919a4847a0a13e7c34c671e1c
Kaan <kaanozmendeveloper@gmail.com>
2025-03-29 11:22:52 +0300

    refactor(api): code split

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,7 @@ import React from "react";
 import { ProductsListResponse } from "@/shared/types/product.types";
 import "@/styles/page.scss";
 import ProductList from "@/components/Product/ProductList";
-
-async function getProducts() {
-  try {
-    const response = await fetch(process.env.BASE_API_URL! + "/Products/List", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.API_TOKEN!}`,
-        "Content-Type": "application/json",
-      },
-    });
-
-    return response.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
+import { getProducts } from "@/services/product";
 
 export default async function HomePage() {
   const products = await getProducts();
@@ -29,9 +14,5 @@ interface IHome {
 }
 
 function Home({ products }: IHome) {
-  return (
-    <React.Fragment>
-      <ProductList products={products.data} />
-    </React.Fragment>
-  );
+  return <ProductList products={products.data} />;
 }
--- src/services/product.ts
+++ src/services/product.ts
@@ -6,7 +6,10 @@ import { api } from "./api";
 
 export const productApi = api.injectEndpoints({
   endpoints: (builder) => ({
-    products: builder.query<ProductsListResponse, ProductsListRequest | undefined>({
+    products: builder.query<
+      ProductsListResponse,
+      ProductsListRequest | undefined
+    >({
       query: (data) => ({
         url: "products/list",
         method: "POST",
@@ -15,5 +18,21 @@ export const productApi = api.injectEndpoints({
     }),
   }),
 });
+
+export const getProducts = async () => {
+  try {
+    const response = await fetch(process.env.BASE_API_URL! + "/Products/List", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.API_TOKEN!}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const { useProductsQuery } = productApi;
